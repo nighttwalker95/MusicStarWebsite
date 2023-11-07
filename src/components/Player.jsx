0628ad45d18ce8f3aef4d09f71d24bd658fd809a
amirreza-eb <nighttwalker95@gmail.com>
2023-11-07 21:56:29 +0330

    added animation to player.jsx

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -9,7 +9,13 @@ import { fadeIn } from "../../variants";
 const Player = () => {
   return (
     <>
-      <div className=" bg-gradient-to-r from-tertiary/70 to-primary/10 backdrop-blur-[15px] h-[112px] felx items-center relative z-40">
+      <motion.div
+        variants={fadeIn("up", 0.1)}
+        initial="hidden"
+        whileInView={"show"}
+        viewport={{ once: false, amount: 0.1 }}
+        className=" bg-gradient-to-r from-tertiary/70 to-primary/10 backdrop-blur-[15px] h-[112px] felx items-center relative z-40"
+      >
         <div className=" container mx-auto flex flex-col justify-between items-center xl:flex-row">
           {/* text & avatar image */}
           <div className=" hidden w-[300px] xl:flex items-center gap-x-4 my-6">
@@ -41,7 +47,7 @@ const Player = () => {
             />
           </div>
         </div>
-      </div>
+      </motion.div>
     </>
   );
 };
