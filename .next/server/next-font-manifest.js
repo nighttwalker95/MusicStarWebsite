357ba89a182b6034fe7537fef1a16c45e4d9d2ab
amirreza-eb <nighttwalker95@gmail.com>
2023-11-07 21:55:29 +0330

    edited gitignore

--- .next/server/next-font-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/Users/mac/Desktop/music website/src/app/layout\":[\"static/media/e87f8d202b2993ba-s.p.woff2\",\"static/media/e1c529c04de64b40-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
