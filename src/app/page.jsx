b115d7a3ea164de82a92090af196ed9405866d31
amirreza-eb <nighttwalker95@gmail.com>
2023-11-08 19:31:17 +0330

    added albums to the Page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,6 @@
 import Hero from "../components/Hero";
 import Player from "../components/Player";
+import Albums from "../components/albums/Albums";
 import Events from "../components/events/Events";
 
 export default function Home() {
@@ -8,6 +9,8 @@ export default function Home() {
       <Hero />
       <Player />
       <Events />
+      <Albums />
+      <div className=" h-[4000px]"></div>
     </main>
   );
 }
