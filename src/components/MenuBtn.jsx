9536b244ca2ec44c21d537d00ba748e115b802f2
amirreza-eb <nighttwalker95@gmail.com>
2023-11-08 18:24:05 +0330

    added some stylings to the MenuBtn

--- src/components/MenuBtn.jsx
+++ src/components/MenuBtn.jsx
@@ -1,5 +1,11 @@
 const MenuBtn = () => {
-  return <div>MenuBtn</div>;
+  return (
+    <div className="group flex flex-col gap-y-2 cursor-pointer xl:hidden group items-end">
+      <div className=" w-7 h-[2px] bg-white transition-all"></div>
+      <div className=" w-4 group-hover:w-7 h-[2px] bg-white transition-all"></div>
+      <div className=" w-7 h-[2px] bg-white transition-all"></div>
+    </div>
+  );
 };
 
 export default MenuBtn;
