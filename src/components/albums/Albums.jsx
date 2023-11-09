6e8d587daf63ca29231a37a12d7bfede8e235548
amirreza-eb <nighttwalker95@gmail.com>
2023-11-09 13:24:17 +0330

    changed albums.jsx configuration

--- src/components/albums/Albums.jsx
+++ src/components/albums/Albums.jsx
@@ -1,5 +1,22 @@
+"use client";
+
+import AlbumSlider from "./AlbumSlider";
+import { motion } from "framer-motion";
+import { fadeIn } from "../../../variants";
+import SectionHeader from "../SectionHeader";
+
 const Albums = () => {
-  return <div>Albums</div>;
+  return (
+    <section id="discography">
+      <div className="container mx-auto">
+        <SectionHeader pretitle="Discography" title="Popular Albums" />
+        {/* album slider */}
+        <div>
+          <AlbumSlider />
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Albums;
