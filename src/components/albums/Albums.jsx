1e9d7f9551a19d1bf6ffe4e483fc823dd12adb2b
amirreza-eb <nighttwalker95@gmail.com>
2023-11-09 19:23:25 +0330

    added animation  for album-slider

--- src/components/albums/Albums.jsx
+++ src/components/albums/Albums.jsx
@@ -11,9 +11,14 @@ const Albums = () => {
       <div className="container mx-auto">
         <SectionHeader pretitle="Discography" title="Popular Albums" />
         {/* album slider */}
-        <div>
+        <motion.div
+          variants={fadeIn("up", 0.4)}
+          initial="hidden"
+          whileInView={"show"}
+          viewport={{ once: false, amount: 0.3 }}
+        >
           <AlbumSlider />
-        </div>
+        </motion.div>
       </div>
     </section>
   );
