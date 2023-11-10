db7d7306e5a39f9438b8db268b3df3128830be8a
amirreza-eb <nighttwalker95@gmail.com>
2023-11-10 12:13:38 +0330

    reFormatted classnames(not Important)

--- src/components/NavMobile.jsx
+++ src/components/NavMobile.jsx
@@ -14,9 +14,9 @@ const NavMobile = () => {
     >
       <div
         onClick={() => setIsOpen(false)}
-        className=" absolute right-4 top-5 cursor-pointer"
+        className="absolute cursor-pointer right-4 top-5"
       >
-        <RiCloseLine className=" text-5xl" />
+        <RiCloseLine className="text-5xl " />
       </div>
       <Nav containerStyles="flex flex-col text-[30px] uppercase font-bold bg-pink-500/10 h-full items-center justify-center gap-y-8" />
     </nav>
--- src/components/blog/Blog.jsx
+++ src/components/blog/Blog.jsx
@@ -1,5 +1,21 @@
-const Blog = () => {
-  return <div>Blog</div>;
+import SectionHeader from "../SectionHeader";
+import PostList from "./PostList";
+
+const getPosts = async () => {
+  const response = await fetch("http://localhost:4000/posts");
+  return response.json();
+};
+
+const Blog = async () => {
+  const posts = await getPosts();
+
+  return (
+    <section className="section" id="blog">
+      <div className="container mx-auto">
+        <SectionHeader pretitle="Our Blog" title="Latest News" />
+      </div>
+    </section>
+  );
 };
 
 export default Blog;
