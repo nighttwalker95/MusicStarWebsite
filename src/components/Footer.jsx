72a4a0e0e0cebf38337412423eba526e0d64d20a
amirreza-eb <nighttwalker95@gmail.com>
2023-11-10 22:11:03 +0330

    completed footer.jsx configuration

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,77 @@
+"use client";
+
+import Link from "next/link";
+import SocialIcons from "./SocialIcons";
+import Nav from "./Nav";
+
+import { motion } from "framer-motion";
+import { fadeIn } from "../../variants";
+import Image from "next/image";
+
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className=" bg-accent section h-[500px] xl:h-auto">
+      <div className="container flex flex-col items-center h-full mx-auto gap-y-8">
+        {/* email link */}
+        <motion.div
+          variants={fadeIn("up", 0.2)}
+          initial="hidden"
+          whileInView={"show"}
+          viewport={{ once: false, amount: 0.3 }}
+        >
+          <Link href="#">
+            <h2 className="text-[24px] lg:text-[38px] font-semibold leading-tight">
+              hello@miareynolds.com
+            </h2>
+          </Link>
+        </motion.div>
+        {/* nav */}
+        <motion.div
+          variants={fadeIn("up", 0.4)}
+          initial="hidden"
+          whileInView={"show"}
+          viewport={{ once: false, amount: 0.3 }}
+        >
+          <Nav
+            containerStyles="flex flex-col xl:flex-row justify-center items-center gap-y-4 xl:gap-x-8 text-sm uppercase font-semibold"
+            linkStyles="hover:text-primary/80 transition-all"
+          />
+        </motion.div>
+        {/* socials */}
+        <motion.div
+          variants={fadeIn("up", 0.6)}
+          initial="hidden"
+          whileInView={"show"}
+          viewport={{ once: false, amount: 0.3 }}
+          className=""
+        >
+          <SocialIcons
+            containerStyles="flex text-[24px] gap-x-8"
+            iconStyles="hover:text-primary/80 transition-all justify-center"
+          />
+        </motion.div>
+        {/* logo */}
+        <motion.div
+          variants={fadeIn("up", 0.8)}
+          initial="hidden"
+          whileInView={"show"}
+          viewport={{ once: false, amount: 0.1 }}
+        >
+          <Link
+            href="#"
+            className=" relative w-[250px] h-[50px] flex transition-all"
+          >
+            <Image
+              src={"/assets/header/logo.svg"}
+              fill
+              alt=""
+              className="object-contain "
+            />
+          </Link>
+        </motion.div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
